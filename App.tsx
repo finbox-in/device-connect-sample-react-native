1ed9b9cbc180a091b1a4b9b3c6b515ae8d8e7a4e
Srikar Reddy <gsrikar2010@gmail.com>
2023-12-15 11:59:28 +0530

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,86 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, { useEffect } from 'react';
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import messaging from '@react-native-firebase/messaging';
+
+import FinBoxRiskSdk from 'react-native-risk-sdk';
+
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
+
+const API_KEY = '';
+const CUSTOMER_ID = 'demo_lender_15121156';
+
+function createUser() {
+  FinBoxRiskSdk.createUser(
+    API_KEY,
+    CUSTOMER_ID,
+    (errorStatus: any) => {
+      // Error Callback
+      console.log('Error status -> ', errorStatus);
+    },
+    (msg: any) => {
+      // Success Callback, Call the periodic sync once the user has been created
+      console.log('Final message', msg);
+      FinBoxRiskSdk.startPeriodicSync(12); //Start the sync periodically after every 12 hour
+    },
+  );
+}
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      console.log('A new FCM message!', remoteMessage.data);
+
+      FinBoxRiskSdk.forwardFinBoxNotificationToSDK(remoteMessage.data);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Button
+            onPress={createUser}
+            title="Create User"
+            color="#841584"
+            accessibilityLabel="Create User"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
